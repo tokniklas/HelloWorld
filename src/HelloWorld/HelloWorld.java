d65eb779d2f7a20f8a7b41a5a76fc3f48187b760
Tiger <Tiger@Tiger-dator>
2012-01-28 12:49:01 +0100

    Second implementation of HelloWorld
    Signed-off-by: Tiger <Tiger@Tiger-dator>

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -6,8 +6,11 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
-
+		for (int i = 0; i < 10; i++) 
+			System.out.println("Hello world!");
+			
+		}
+		
 	}
 
-}
+
